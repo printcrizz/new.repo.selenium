ae6570b7ebf72943199a50e4338afca9b1f182e5
printcrizz <criarave@gmail.com>
2018-07-05 00:08:41 -0400

    add first test

---
+++ selenium-app/src/main/groovy/seleniumUtils.groovy
@@ -0,0 +1,4 @@
+import org.openqa.selenium.Dimension
+import org.openqa.selenium.JavascriptExecutor
+import org.openqa.selenium.WebDriver
+
--- selenium-app/src/main/java/com/Cristian/app/App.java
+++ selenium-app/src/main/java/com/Cristian/app/App.java
@@ -1,13 +1,22 @@
 package com.Cristian.app;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    @Test
+        public void test1(){
+        WebDriver driver= new FirefoxDriver();
+        driver.get("http://www.google.com");
+        driver.close();
+        driver.quit();
+
     }
+
 }
