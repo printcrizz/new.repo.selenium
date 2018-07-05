32b6304719aa11753eaff5ec40e980984866e39b
printcrizz <criarave@gmail.com>
2018-07-04 22:01:45 -0400

    project structure

---
+++ selenium-app/src/main/java/com/Cristian/app/App.java
@@ -0,0 +1,13 @@
+package com.Cristian.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
